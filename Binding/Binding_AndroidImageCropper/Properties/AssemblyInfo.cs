b5e85be61af118d9851259e5bdae3abdc6c05d01
Carlos Mendoza <inhack20@gmail.com>
2020-05-09 11:13:00 -0400

    feat(android): bidding se actualiza libreria a androidx

--- Binding/Binding_AndroidImageCropper/Properties/AssemblyInfo.cs
+++ Binding/Binding_AndroidImageCropper/Properties/AssemblyInfo.cs
@@ -22,9 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
